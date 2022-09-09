64a31a975a4bcd90a571b037af09c05b11d7437f
Cathyrt <cathrinert20@icloud.com>
2022-09-09 06:29:24 -0700

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Print the numbers
+ * main - Print if the number is positive, zero, or negative
  *
+ * Description: using the main function
  * Return: Always (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '10'; i++)
+	for (i = '0'; i < 10; i++)
 	{
 		printf("%i", i);
 	}
