dfea59f7196eb2d33c8c17ad36f4261f6f23a679
Cathyrt <cathrinert20@icloud.com>
2023-04-12 12:05:19 -0700

    Advanced binary

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - Searches for a value in a sorted array of integers.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The first index of the value in the array, otherwise -1.
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t i, mid;
+	int res;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+		printf("%d%s", array[i], i == size - 1 ? "" : ", ");
+
+	mid = (size - 1) / 2;
+
+	if (array[mid] == value)
+	{
+		if (mid == 0 || array[mid - 1] != value)
+			return (mid);
+	}
+	if (size == 1)
+		return (-1);
+
+	if (array[mid] < value)
+	{
+		res = advanced_binary(array + mid + 1, size - mid - 1, value);
+		return (res == -1 ?(size_t)-1 : mid + 1 + res);
+	}
+	else
+		return (advanced_binary(array, mid + 1, value));
+}
